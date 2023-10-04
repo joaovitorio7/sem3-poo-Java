5e1c6de25a4f5b9e17871108ae34c9fe7c4a0ba6
Joao <joaoprodutor92@gmail.com>
2023-10-04 19:16:51 -0300

    finaliza finals

--- src/main/java/tech/devinhouse/clamedv2/aula02/finals/Teste.java
+++ src/main/java/tech/devinhouse/clamedv2/aula02/finals/Teste.java
@@ -1,11 +1,14 @@
 package tech.devinhouse.clamedv2.aula02.finals;
 
+import tech.devinhouse.clamedv2.aula02.finals.Pessoa;
+
 public class Teste {
+
     public static void main(String[] args) {
 
-        final int idade = 30;
-        final String nome =
-
-        idade = 40;
+        final int idade = 30; // constante
+        final String nome = "james kirk";
+        final Pessoa john = new Pessoa("john", 123L, "black");
+        john.setNomeSocial("john snow know nothing");
     }
 }
