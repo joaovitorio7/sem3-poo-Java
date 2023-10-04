194df4a0c05753e1268062c4db5ceca1f33cb808
Joao <joaoprodutor92@gmail.com>
2023-10-04 18:09:21 -0300

    inicializa finals

---
+++ src/main/java/tech/devinhouse/clamedv2/aula02/finals/Teste.java
@@ -0,0 +1,11 @@
+package tech.devinhouse.clamedv2.aula02.finals;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        final int idade = 30;
+        final String nome =
+
+        idade = 40;
+    }
+}
